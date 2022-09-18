b6aaece66fec021bde6af8f2e7bab70a626f63e3
Максим Богачёв <m.bogachev.prog@solo-it.ru>
2022-09-18 16:11:34 +0400

    Написана логика main.php

--- main.php
+++ main.php
@@ -1,1 +1,31 @@
-<??>
+<?
+include_once "Farm.php";
+
+$animals = [
+	"cows" => [
+		"count" = 10,
+		"resourseCollected" = 0,
+		"lastWeeksCollected" = 0
+	],
+	"chickens" => [
+		"count" = 20,
+		"resouseCollected" = 0,
+		"lastWeeksCollected" = 0 
+	]
+];
+
+$farm = new Farm($animals);
+for($i=1;$i<=7;$i++){
+	echo "День №{$i}";
+	$farm->collectResourses($i);
+	echo "Собраны яйца и молоко";
+}
+$farm->getCountResousesForWeek();
+$farm->buyAnimals(1,5);
+for($i=1;$i<=7;$i++){
+	echo "День №{$i}";
+	$farm->collectResourses($i);
+	echo "Собраны яйца и молоко";
+}
+$farm->getCountResousesForWeek();
+?>
