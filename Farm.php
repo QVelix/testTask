5224cec8e3f795e043e6d7fe54d0cf7f7843c6d9
Максим Богачёв <m.bogachev.prog@solo-it.ru>
2022-09-18 16:31:01 +0400

    Исправление ошибок для консоли

--- Farm.php
+++ Farm.php
@@ -1,19 +1,19 @@
-<?
+<?php
 class Farm{
 	private $animals; //Массив животных
 	private $collectDay = 0;//День когда собирали с животных "ресурсы"
 	function __construct($animalsArray){
-	$this->animals = $animalsArray
-		foreach($this->animals as $i->$arrTypeAnimal){
-			echo "{$i} = {$arrTypeAnimal["count"]}";
+	$this->animals = $animalsArray;
+		foreach($this->animals as $i=>$arrTypeAnimal){
+			echo "{$i} = {$arrTypeAnimal["count"]}\n";
 		}
 	}
 	
 	public function buyAnimals($countCows, $countChickens){
 		$this->animals["cows"]["count"]+=$countCows;
-		echo "Купили {$countCows} коров";
+		echo "Купили {$countCows} коров\n";
 		$this->animals["chickens"]["count"]+=$countChickens;
-		echo "Купили {$countChines} куриц";
+		echo "Купили {$countChickens} куриц\n";
 	}
 	
 	public function collectResourses($day){
@@ -21,24 +21,24 @@ class Farm{
 			echo "Не трогайти животных, они устали! Вы сегодня уже собрали всё что могли.";
 			return 0;
 		}
-		foreach($this->animals as $typeAnimal->$arrTypeAnimal){
+		foreach($this->animals as $typeAnimal=>$arrTypeAnimal){
 			if($typeAnimal == "cows"){
-				for($i = 0;$i<$arrTypeAnimal["count"]){
-					$this->animals[$i]["resourseCollected"]+=rand(8, 12);
+				for($i = 0;$i<$arrTypeAnimal["count"];$i++){
+					$this->animals["cows"]["resourseCollected"]+=rand(8, 12);
 				}
 			}
 			if($typeAnimal == "chickens"){
-				for($i=0;$i<$arrTypeAnimal["count"]){
-					$this->animals[$i]["resourseCollected"]+=rand(0,1);
+				for($i=0;$i<$arrTypeAnimal["count"];$i++){
+					$this->animals["chickens"]["resourseCollected"]+=rand(0,1);
 				}
 			}
 		}
 	}
 	
 	public function getCountResousesForWeek(){
-		echo "За неделю собрано ".($this->animals["cows"]["resourseCollected"]-$this->animals["cows"]["lastWeeksCollected"])." литров молока";
+		echo "За неделю собрано ".($this->animals["cows"]["resourseCollected"]-$this->animals["cows"]["lastWeeksCollected"])." литров молока\n";
 		$this->animals["cows"]["lastWeeksCollected"] = $this->animals["cows"]["resourseCollected"];
-		echo "За неделю собрано ".($this->animals["chickens"]["resourseCollected"]-$this->animals["chickens"]["lastWeeksCollected"])." яиц";
+		echo "За неделю собрано ".($this->animals["chickens"]["resourseCollected"]-$this->animals["chickens"]["lastWeeksCollected"])." яиц\n";
 		$this->animals["chickens"]["lastWeeksCollected"] = $this->animals["chickens"]["resourseCollected"];
 	}
 }
--- main.php
+++ main.php
@@ -1,31 +1,31 @@
-<?
+<?php
 include_once "Farm.php";
 
 $animals = [
 	"cows" => [
-		"count" = 10,
-		"resourseCollected" = 0,
-		"lastWeeksCollected" = 0
+		"count" => 10,
+		"resourseCollected" => 0,
+		"lastWeeksCollected" => 0
 	],
 	"chickens" => [
-		"count" = 20,
-		"resouseCollected" = 0,
-		"lastWeeksCollected" = 0 
+		"count" => 20,
+		"resourseCollected" => 0,
+		"lastWeeksCollected" => 0 
 	]
 ];
 
 $farm = new Farm($animals);
 for($i=1;$i<=7;$i++){
-	echo "День №{$i}";
+	echo "День №{$i}\n\n";
 	$farm->collectResourses($i);
-	echo "Собраны яйца и молоко";
+	echo "Собраны яйца и молоко\n";
 }
 $farm->getCountResousesForWeek();
 $farm->buyAnimals(1,5);
 for($i=1;$i<=7;$i++){
-	echo "День №{$i}";
+	echo "День №{$i}\n\n";
 	$farm->collectResourses($i);
-	echo "Собраны яйца и молоко";
+	echo "Собраны яйца и молоко\n";
 }
 $farm->getCountResousesForWeek();
 ?>
