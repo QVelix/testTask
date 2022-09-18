7a7ca289982d2ac2f1204b1fbd1618f70a71b9c5
Максим Богачёв <m.bogachev.prog@solo-it.ru>
2022-09-18 15:40:56 +0400

    Написание конструктора для Farm

--- Farm.php
+++ Farm.php
@@ -1,1 +1,11 @@
-<??>
+<?
+public class Farm{
+	private $animals;
+	__construct($animalsArray){
+	$this->animals = $animalsArray
+		foreach($this->animals as $i->arrTypeAnimal){
+			echo "{$i} count {$arrTypeAnimal["count"]}";
+		}
+	}
+}
+?>
