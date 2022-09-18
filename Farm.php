2cb3bc116dd06ab07d21e2dfb49e73b4f39997b0
Максим Богачёв <m.bogachev.prog@solo-it.ru>
2022-09-18 15:50:43 +0400

    Написание функций для сбора ресурсов и покупки животных

--- Farm.php
+++ Farm.php
@@ -1,10 +1,24 @@
 <?
-public class Farm{
-	private $animals;
-	__construct($animalsArray){
+class Farm{
+	private $animals; //Массив животных
+	private $collectDay = 0;//День когда собирали с животных "ресурсы"
+	function __construct($animalsArray){
 	$this->animals = $animalsArray
 		foreach($this->animals as $i->arrTypeAnimal){
-			echo "{$i} count {$arrTypeAnimal["count"]}";
+			echo "{$i} = {$arrTypeAnimal["count"]}";
+		}
+	}
+	
+	public function buyAnimals($countCows, $countChickens){
+		$this->animals["cows"]["count"]+=$countCows;
+		echo "Купили {$countCows} коров";
+		$this->animals["chickens"]["count"]+=$countChickens;
+		echo "Купили {$countChines} куриц";
+	}
+	
+	public function collectResourses($day){
+		if($this->collectDay == $day){
+			echo "Не трогайти животных, они устали! Вы сегодня уже собрали всё что могли.";
 		}
 	}
 }
