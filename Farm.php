3adf9c73b9e6ea490ee1c0ceed23c1ca99fa17d3
Максим Богачёв <m.bogachev.prog@solo-it.ru>
2022-09-18 16:11:26 +0400

    Сделан вывод собранных ресурсов за неделю

--- Farm.php
+++ Farm.php
@@ -4,7 +4,7 @@ class Farm{
 	private $collectDay = 0;//День когда собирали с животных "ресурсы"
 	function __construct($animalsArray){
 	$this->animals = $animalsArray
-		foreach($this->animals as $i->arrTypeAnimal){
+		foreach($this->animals as $i->$arrTypeAnimal){
 			echo "{$i} = {$arrTypeAnimal["count"]}";
 		}
 	}
@@ -19,7 +19,27 @@ class Farm{
 	public function collectResourses($day){
 		if($this->collectDay == $day){
 			echo "Не трогайти животных, они устали! Вы сегодня уже собрали всё что могли.";
+			return 0;
 		}
+		foreach($this->animals as $typeAnimal->$arrTypeAnimal){
+			if($typeAnimal == "cows"){
+				for($i = 0;$i<$arrTypeAnimal["count"]){
+					$this->animals[$i]["resourseCollected"]+=rand(8, 12);
+				}
+			}
+			if($typeAnimal == "chickens"){
+				for($i=0;$i<$arrTypeAnimal["count"]){
+					$this->animals[$i]["resourseCollected"]+=rand(0,1);
+				}
+			}
+		}
+	}
+	
+	public function getCountResousesForWeek(){
+		echo "За неделю собрано ".($this->animals["cows"]["resourseCollected"]-$this->animals["cows"]["lastWeeksCollected"])." литров молока";
+		$this->animals["cows"]["lastWeeksCollected"] = $this->animals["cows"]["resourseCollected"];
+		echo "За неделю собрано ".($this->animals["chickens"]["resourseCollected"]-$this->animals["chickens"]["lastWeeksCollected"])." яиц";
+		$this->animals["chickens"]["lastWeeksCollected"] = $this->animals["chickens"]["resourseCollected"];
 	}
 }
 ?>
